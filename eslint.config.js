884aa816bf069e9da025cbc3241209c9f7124c31
Paz <marayam1@ucenfotec.ac.cr>
2025-12-02 14:47:45 -0600

    new: eslint config file
    Aplica las reglas recomendadas de JavaScript
    Configura variables globales del navegador y Node.js
    Agrega reglas personalizadas (punto y coma obligatorio, comillas simples, etc.)
    Ignora carpetas que no deben analizarse

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'warn',
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+    },
+  },
+  {
+    ignores: ['dist/**', 'node_modules/**', '.astro/**'],
+  },
+];
